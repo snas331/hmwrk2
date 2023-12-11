72c66ff2e71a5df68156ac9969c7430d05938dd6
yakov23 <yakovoberderfer13@gmail.com>
2023-12-11 22:54:28 +0300

    file ignored

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int na4s4et = 110;
+        int popolnenie = 1000;
+        int bonus;
+
+        if(popolnenie >= 1000) {
+            bonus = popolnenie / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println( bonus );
+
+
+
+
+    }
+}
